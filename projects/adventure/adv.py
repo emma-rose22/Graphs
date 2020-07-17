7e30debefad2843596c283aca942a5cdf9a33e81
emma-rose22 <57078967+emma-rose22@users.noreply.github.com>
2020-07-17 13:01:52 -0400

    partial

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -81,8 +81,6 @@ def make_room_dict(starter_room):
     the_room[starter_room.name] = room_directions
     return the_room
 
-print('what', player.current_room.name)
-
 def graph_all_rooms(starter_room):
     #this is a DFT
     #the master graph
@@ -94,26 +92,134 @@ def graph_all_rooms(starter_room):
 
     while s.size() > 0:
         current_node = s.pop()
-        print('current node:', current_node)
         if current_node not in visited:
             visited.add(current_node)
             #add room's directions and room to all_rooms
-            all_rooms[current_node.name] = make_room_dict(current_node)
-            print('added room:', all_rooms[current_node.name])
+            all_rooms.update(make_room_dict(current_node))
+
             #for each of the available directions in that room
-            for direction_list in all_rooms[current_node.name].values():
-                for direction in direction_list.keys():
-                    print('direction:', direction)
-                    #if we can travel there 
-                    player.travel(direction)
-                    #print('player room:', player.current_room)
-                    #add that room to the stack of rooms to visit
-                    s.push(player.travel(direction))
+            #I need to be able to move back to the room I was just in after
+            #checking out the surrounding rooms
+            for direction in all_rooms[current_node.name].keys():
+                #is now the time to add room names to the dictionary??
+      
+
+                #if we can travel there 
+           
+                #print('player room:', player.current_room)
+                #add that room to the stack of rooms to visit
+                s.push(player.current_room.get_room_in_direction(direction))
+
+                player.travel(direction)
+                s.push(player.current_room.get_room_in_direction(direction))
+                #I want to go back
+                if direction == 'n':
+                    player.travel('s')
+                if direction == 's':
+                    player.travel('n')
+                if direction == 'e':
+                    player.travel('w')
+                if direction == 'w':
+                    player.travel('e')
+                
+    return all_rooms
+#when I move in a direction, I take away the ability to move in the other cardinal directions
+#print('first graph:', graph_all_rooms(player.current_room))
+
+def fill_in_graph(starter_room):
+    #go get the graph of the maze
+    all_rooms = graph_all_rooms(starter_room)
+    visited = {}
+    q = Queue()
+    #create path to return as traversal
+    path = []
+    q.enqueue(starter_room)
+
+    while q.size() > 0:
+        #take out the first one
+        current_node = q.dequeue()
+        #for each possible direction of that room
+        for direction in all_rooms[current_node.name].keys():
+            #if it is not visited yet
+            if all_rooms[current_node.name][direction] == '?':
+                #append that direction to our path (we are about to move there)
+                path.append(direction)
+                #move to that direction
+                player.travel(direction)
+                #add that room to the list of rooms???
+                q.enqueue(player.current_room)
+                #rename the direction in that node to be the room name 
+                all_rooms[current_node.name][direction] = player.current_room.name
+
+                if direction == 'n':
+                    player.travel('s')
+                if direction == 's':
+                    player.travel('n')
+                if direction == 'e':
+                    player.travel('w')
+                if direction == 'w':
+                    player.travel('e')
+
+    print('the graph:', all_rooms)
     return all_rooms
 
-#print('trying to move:', player.travel('n'))
+def create_path(starter_room):
+    graph = fill_in_graph(starter_room)
+    q = Queue()
+    visited = {}
+    path = []
+    #get count of graph
 
-print('first graph:', graph_all_rooms(player.current_room))
+    q.enqueue(starter_room)
+
+    graph_size = 0
+    for k, v in graph.items():
+        graph_size = graph_size + len(v)
+
+    #do a breadth first traversalish
+    #get graph[current_room] cardinal directions
+    #    if one hasnt been visited
+    #    add to visited
+    #    add cardinal step to path
+    #    move player in that direction
+    #    repeat
+
+    #if all adjacent rooms have been visited
+    #    pick a direction != to the one you came in
+
+    #if len visited == len of graph:
+        #return path
+
+    while len(visited) < graph_size:
+        current = q.dequeue()
+        #get all directions, if not in visited put in list
+        #take first item out of list to move to 
+        not_visited_rooms = []
+        for direction in graph[current.name].keys():
+            if graph[current.name][direction] not in visited:
+                not_visited_rooms += direction
+        if len(not_visited_rooms) > 0:
+            visited[current.name] = [direction] 
+            path += direction
+            player.travel(direction)
+            q.enqueue(player.current_room)
+        else:
+            #this is if we reach a dead end
+            opposite_direction = path[-1]
+            room_exits = list(current.get_exits())
+            options = room_exits.remove(opposite_direction)
+            move_to = random.choice(options)
+            path += move_to
+            player.travel(move_to)
+            q.enqueue(player.current_room)
+    return path
+
+        
+
+    
+
+traversal_path = create_path(player.current_room)
+print('dummy_path:', traversal_path)
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -124,8 +230,9 @@ visited_rooms.add(player.current_room)
 for move in traversal_path:
     player.travel(move)
     visited_rooms.add(player.current_room)
-for i in visited_rooms:
-    print('room:', i.name)
+# for i in visited_rooms:
+#     print('room:', i.name)
+#     print('description:', i.description)
 
 
 if len(visited_rooms) == len(room_graph):
